86f3892ba7f1d5eb0237d10a7e68d966c18786f3
chet1211221 <chet1211221@gmail.com>
2014-06-14 20:59:20 +0430

    commented on the page struct and renderTemplate.

--- webserver/page.go
+++ webserver/page.go
@@ -1,7 +1,8 @@
 package webserver
 
 //Page is a struct for information to be displayed on the webpages
+//Additional fields will be added as I figure out which elements are needed
 type Page struct {
-	Title string
-	Body  []byte
+	Title string //Title of webpage
+	Body  []byte //Body in byte form.
 }
--- webserver/renderTemplate.go
+++ webserver/renderTemplate.go
@@ -5,6 +5,21 @@ import (
 	"net/http"
 )
 
+//renderTemplate using html/template to create a templates from a saved .html
+//file.
+//Inputs: w is a http.ResponseWriter that is an interface used by an HTTP
+//handler to construct an HTTP response.
+//Input: tmpl is a string that points to the .html file for the template.
+//Adding the .html is not required and has the potenial to mess up the function.
+//Input: p is a Page struct.
+//Output: tmpl the template with the info from the Page struct is written to
+//w the http.ResponseWriter.
+//Error: If the template is not found, then the http code 500 is written to
+//w the http.ResponseWriter.
+//Process: html/templates ExecuteTemple is called using the approperiate
+//ResponseWriter, the tmpl string plus the .html file extention, and the
+//approperiate Page struct. If ExecuteTemplate reruns any errors, then a http
+//code 500 is written to the http.ResponseWriter.
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
